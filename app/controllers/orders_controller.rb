6184d727e0750cf2dcae7ab56352425d32c64805
ArioShaman <ariosha6@gmail.com>
2020-07-22 06:22:20 +0300

    personal data validation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,17 +3,25 @@ class OrdersController < ApplicationController
 
     def create
         if @guest_user.present?
+            puts ">>> #{ap form_params}"
             personalData = PersonalData.create(form_params)
-            order = Order.create(personal_data: personalData)
-            basket_list = @guest_user.baskets
-            basket_list.each do |basket|
-                basket.update(order: order)
+            if personalData.valid?
+                order = Order.create(personal_data: personalData)
+                basket_list = @guest_user.baskets
+                basket_list.each do |basket|
+                    basket.update(order: order)
+                end
+                OrderMailer.send_order_data(order).deliver
+                render json: {
+                    msg: 'Ok',
+                    error: false
+                }
+            else
+               render json: {
+                    msg: "Blank params",
+                    error: true
+                } 
             end
-            OrderMailer.send_order_data(order).deliver
-            render json: {
-                msg: 'Ok',
-                error: false
-            }
         else
             render json: {
                 msg: "User not found",
@@ -24,10 +32,15 @@ class OrdersController < ApplicationController
 
 private
     def form_params
-        params.require(:formData).permit(:full_name, :phone, :email, :address, :comment).merge(guest_user_id: @guest_user.id)
+            params.require(:formData).permit(:full_name, :phone, :email, :address, :comment).merge(guest_user_id: @guest_user.id)
     end
 
     def set_guest_user
         @guest_user = GuestUser.where(token: params[:guest_user_id]).first
     end
+    ActionController::Parameters.action_on_unpermitted_parameters = :raise
+
+    rescue_from(ActionController::UnpermittedParameters) do |pme|
+        render json: { error:  { unknown_parameters: pme.params } }, status: :bad_request
+    end    
 end
--- app/models/personal_data.rb
+++ app/models/personal_data.rb
@@ -1,3 +1,4 @@
 class PersonalData < ApplicationRecord
-    belongs_to :guest_user, optional: true 
+    belongs_to :guest_user, optional: true
+    validates :full_name, :phone, :email, :address, presence: true
 end
